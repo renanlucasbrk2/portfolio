0e130c777d36517c6209aa34982a7e322ca70dbc
renanLS <renanlucas231@gmail.com>
2024-08-27 00:21:34 -0300

    att27.08.2024

---
+++ src/main.js
@@ -0,0 +1,7 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     new TypeIt(".animated", {
+//         speed: 100,
+//         strings: "Um desenvolvedor front-end apaixonado por criar interfaces modernas e funcionais.Com habilidades em JavaScript, HTML, CSS, ReactJS e um foco especial em UI/UX.",
+//         loop: true
+//     }).go()
+// })
